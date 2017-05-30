97a28e7dbf7e128a16252572b59d0721827095de
Kirill Pisarenko <assent222@gmail.com>
2017-05-30 22:41:57 +0300

    init lab

--- src/app.js
+++ src/app.js
@@ -2,11 +2,12 @@ const css = require('./app.scss');
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import bulbasaur from './images/bulbasaur.jpg';
 
 ReactDOM.render(
     <div>
         <h1>Hello, app page!</h1>
-
+        <img src={bulbasaur} />
     </div>,
     document.getElementById('root')
 );
